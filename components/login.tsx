2b304a17e2f3bf5dd4cb9e942f33616b190d1200
qjawns0222 <33923212+qjawns0222@users.noreply.github.com>
2022-04-19 17:50:09 +0900

    data-fetch miss

--- components/login.tsx
+++ components/login.tsx
@@ -1,5 +1,21 @@
-const Login = () => {
-  return <div>login:component</div>;
+import type { GetServerSideProps, NextPage } from "next";
+import LoginContainer from "../containers/loginContainer";
+const Login = ({ data }: { data: Object }) => {
+  return (
+    <div>
+      <LoginContainer />
+    </div>
+  );
+};
+export const getServerSideProps: GetServerSideProps = async () => {
+  // Fetch data from external API
+  const data1 = await fetch(
+    "https://raw.githubusercontent.com/qjawns0222/caremaker/next%2Bts-6%EC%9D%BC%EC%B0%A8/carddata.json"
+  );
+  const data = await data1.json();
+
+  // Pass data to the page via props
+  return { props: { data } };
 };
 
 export default Login;
