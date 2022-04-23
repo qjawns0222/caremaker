77abf5ee7b0c3ac128ebb4045a9e895187bfdde8
qjawns0222 <33923212+qjawns0222@users.noreply.github.com>
2022-04-23 19:17:14 +0900

    redux error modify

--- data.ts
+++ data.ts
@@ -5,8 +5,6 @@ export const Dafe = async () => {
     const data = axios.get(
       "https://raw.githubusercontent.com/qjawns0222/caremaker/next%2Bts-6%EC%9D%BC%EC%B0%A8/carddata.json"
     );
-
-    return data;
   } catch (e: any) {
     console.log(e.response.status);
   }
